8439f96d35ff0a62fcea9adde6c4f91de5a6d33c
Joel <jojabeet@gmail.com>
2024-08-10 00:59:59 -0500

    dependencies updated

--- lib/screens/create_dream.dart
+++ lib/screens/create_dream.dart
@@ -25,6 +25,7 @@ class _CreateDreamPageState extends State<CreateDreamPage> {
           children: [
             TextField(
               controller: _dreamController,
+              // style: TextStyle(backgroundColor: Colors.red),
               maxLines: 10,
               decoration: const InputDecoration(
                 hintText: 'Describe your dream...',
@@ -39,8 +40,48 @@ class _CreateDreamPageState extends State<CreateDreamPage> {
                   Navigator.pop(context);
                 }
               },
-              child: const Text('Analyze Dream with AI'),
+              style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(Theme.of(context).colorScheme.secondary), // Background color
+                  padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0), // Padding inside the button
+                  ),
+                  textStyle: WidgetStateProperty.all<TextStyle>(
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.inversePrimary)  , // Text style
+                  ),
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                    ),
+                  ), 
+                ),
+              child: const Text('Log Dream'),
             ),
+            ElevatedButton(
+              onPressed: () {
+                if (_dreamController.text.isNotEmpty) {
+                  widget.onDreamAdded(_dreamController.text);
+                  Navigator.pop(context);
+                }
+              },
+              
+              style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(Colors.lightBlue), // Background color
+                  padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0), // Padding inside the button
+                  ),
+                  textStyle: WidgetStateProperty.all<TextStyle>(
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold), // Text style
+                  ),
+                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                    ),
+                  ), 
+                ),
+              child: const Text('Analyze Dream with AI'),
+                         
+              ),
+
           ],
         ),
       ),
